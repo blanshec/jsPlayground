b4341da85e60eea0388794402ab6a58d68d5fac4
Allie <alinaabdurshina@gmail.com>
2019-02-21 02:29:18 +0300

    cleanup +task04

--- resources/js/tasklist.js
+++ resources/js/tasklist.js
@@ -1,13 +1,23 @@
-function fClearLog() {
-    document.getElementById('jsLog').innerHTML = "";
+"use strict";
+
+function getLogWindow() {
+    let logWindow = document.getElementById("jsLog");
+    return logWindow;
 }
 
-function fZad01() {
+function clearLog() {
+    getLogWindow().innerHTML = "";
+}
+
+function completeTask01() {
     var surname = prompt("Enter your name", "0");
     var gender = prompt("Enter your gender", "0");
     var age = prompt("Enter your age", "0");
 
-    var result = confirm("SURNAME: " + surname + "\n" + "GENDER: " + gender + "\n" + "AGE: " + age + "\nRight?");
+    var result = confirm("SURNAME: " + surname +
+        "\n" + "GENDER: " + gender +
+        "\n" + "AGE: " + age +
+        "\nRight?");
 
     if (!result) {
         alert("Something went wrong...");
@@ -16,30 +26,31 @@ function fZad01() {
     }
 }
 
-function fZad02() {
-    var num01 = prompt("Enter first number", "0");
-    var x = parseInt(num01);
-    var num02 = prompt("Enter secont number", "0");
-    var y = parseInt(num02);
-    var str = prompt("Enter operation symbol", "0");
+function completeTask02() {
+    const num01 = prompt("Enter first number", "0");
+    let x = parseInt(num01);
+    const num02 = prompt("Enter secont number", "0");
+    let y = parseInt(num02);
+    const str = prompt("Enter operation symbol", "0");
     const opera = ['+', '-', '*', '/'];
 
-    if (!opera.includes(str)) {
+    if (opera.includes(str)) {
+        if (str === "+") {
+            alert(x + y);
+        } else if (str === "*") {
+            alert(x * y);
+        } else if (str === "-") {
+            alert(x - y);
+        } else if (str === "/") {
+            if (y == "0") alert("Cannot divide by ZERO ;)"); else alert(x / y);
+        }
+    } else {
         alert("Wrong symbol");
-    } else if (str === "+") {
-        alert(x + y);
-    } else if (str === "*") {
-        alert(x * y);
-    } else if (str === "-") {
-        alert(x - y);
-    } else if (str === "/") {
-        if (y == "0") alert("Cannot divide by ZERO ;)"); else alert(x / y);
     }
 }
 
-function fZad03() {
-    const demo = document.getElementById('jsLog');
-    demo.innerHTML = "";
+function completeTask03() {
+    const demo = getLogWindow();
 
     const rows = `<table>
 					<tr> <td class='r1'></td> <td class='r1'></td> <td class='r2'></td> <td class='r1'></td> <td class='r1'></td> <td class='r1'></td> <td class='r2'></td> <td class='r1'></td> <td class='r1'></td> <td class='r1'></td></tr>
@@ -53,10 +64,20 @@ function fZad03() {
 					<tr> <td class='r1'></td> <td class='r1'></td> <td class='r2'></td> <td class='r1'></td> <td class='r1'></td> <td class='r1'></td> <td class='r2'></td> <td class='r1'></td> <td class='r1'></td> <td class='r1'></td></tr>
 					<tr> <td class='r1'></td> <td class='r2'></td> <td class='r1'></td> <td class='r1'></td> <td class='r1'></td> <td class='r2'></td> <td class='r1'></td> <td class='r1'></td> <td class='r1'></td> <td class='r2'></td></tr>
 				 </table>`;
-    demo.insertAdjacentHTML('afterbegin', "<link href='../styles/tablecolors.css' rel='stylesheet'>");
+    demo.insertAdjacentHTML('afterbegin',
+        "<link href='resources/css/task03/colored-table.css' rel='stylesheet'>");
     demo.insertAdjacentHTML('beforeend', rows);
 }
 
-function fZad04() {
+function completeTask04() {
+    const arrayDays = ["Sunday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const arrayMonths = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
 
+    let date;
+    date = new Date();
+
+    const demo = getLogWindow();
+    demo.innerHTML = "Today is " + arrayDays[date.getDay()] + ", "
+        + date.getDate() + " of " + arrayMonths[date.getMonth()] + " " + date.getFullYear();
 }
